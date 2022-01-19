fded064feb07c1915681dd5752678c185f6309e0
notorium <74527707+notorium@users.noreply.github.com>
2022-01-19 15:30:10 +0900

    renameブランチで増えたファイルを追加

---
+++ Alexa_cooperation_db.php
@@ -0,0 +1,41 @@
+<?php
+try {
+    //ユーザーがAlexaと連携しているかどうか検索する
+    $sql = "SELECT Alexa_id FROM user
+    WHERE id = :id";
+
+    $stm = $pdo->prepare($sql);
+    $stm->bindValue(':id', $_SESSION['user_id'], PDO::PARAM_INT);
+    $stm->execute();
+    $result = $stm->fetch(PDO::FETCH_COLUMN);
+    if ($result == NULL) : ?>
+
+        <form action="alexa_password_set.php" method="post">
+            <input type="number" name="pass_id" placeholder="6桁の数字を入力してください。" min="100000" max="999999" required>
+            <input type="submit" title="設定を完了します。" value="設定">
+        </form>
+
+        <?php
+        //既に設定されているパスワードがないか検索する
+        $sql = "SELECT pass_id FROM Alexa_coop
+        WHERE user_id = :user_id";
+
+        $stm = $pdo->prepare($sql);
+        $stm->bindValue(':user_id', $_SESSION['user_id'], PDO::PARAM_INT);
+        $stm->execute();
+        $result = $stm->fetch(PDO::FETCH_COLUMN);
+        if (!$result) {
+            $result = "現在設定されている番号はありません。";
+        }
+        ?>
+
+        <p>現在設定されている番号 : <?= $result ?></p>
+    <?php
+    else : ?>
+        <p>既にAlexaと連携しています。</p>
+<?php
+    endif;
+} catch (Exception $e) {
+    echo "エラーが発生しました。";
+}
+?>
---
+++ items_list.php
@@ -0,0 +1,67 @@
+<?php
+$week = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+$week_jp = ["日", "月", "火", "水", "木", "金", "土"];
+date_default_timezone_set('Asia/Tokyo');
+
+try {
+    //ユーザーが登録している持ち物を検索する
+    $sql = "SELECT a.name, b.item_id, b.days, b.notice_datetime 
+    FROM item a, user_item
+    WHERE b.user_id = :id 
+    AND a.id = b.item_id";
+
+    $stm = $pdo->prepare($sql);
+    $stm->bindValue(':id', $_SESSION["user_id"], PDO::PARAM_INT);
+    $stm->execute();
+    $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+    //リストで表示する
+    if (isset($result)) : ?>
+        <table class="full-width">
+            <th>持ち物</th>
+            <th></th>
+            <th>曜日</th>
+            <th>日時</th>
+            <?php
+            foreach ($result as $row) : ?>
+                <tr>
+                    <td><?= $row['name'] ?></td>
+                    <td>
+                        <form action="registration_items.php" method="post"><input type="hidden" name="item_id" value="<?= $row['item_id'] ?>"><input type="submit" value="変更" class="con"></form>
+                        <form action="delete_items.php" method="post"><input type="hidden" name="item_id" value="<?= $row['item_id'] ?>"><input type="submit" value="削除" class="con"></form>
+                    </td>
+
+                    <?php
+                    if (preg_match('/ALL/u', $row["days"])) {
+                        $days = "毎日";
+                    } else {
+                        $days = "";
+                        for ($i = 0; $i < count($week); $i++) {
+                            //文字列が含まれるなら
+                            if (preg_match('/' . $week[$i] . '/u', $row["days"])) {
+                                $days .= $week_jp[$i];
+                            }
+                        }
+                    }
+                    ?>
+
+                    <td><?= $days ?></td>
+                    <?php
+                    if (isset($row['notice_datetime'])) : ?>
+                        <td><?= date("Y年m月d日 H時i分", strtotime($row['notice_datetime'])) ?></td>
+                    <?php
+                    else : ?>
+                        <td>,</td>
+                    <?php
+                    endif; ?>
+                </tr>
+            <?php
+            endforeach; ?>
+        </table>
+    <?php
+    else : ?>
+        <p>持ち物は登録されていません。</p>
+<?php
+    endif;
+} catch (Exception $e) {
+    echo "エラーが発生しました。";
+}
