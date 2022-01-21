c6b1f70901a2ec535f080fa493fd88812427f945
besurand <fujita.shun.t20ohf@gmail.com>
2022-01-21 11:12:41 +0900

    time_set.php,time_top.phpの再構築

--- time_set.php
+++ time_set.php
@@ -1,8 +1,11 @@
 <?php session_start(); ?>
 <?php
 
-require_once("dbconnect.php");
+require 'dbconnect.php';
+date_default_timezone_set('Asia/Tokyo');
 
+require 'search_time.php';
+/*
 try{
     //登録されている時間を拾うsql
     $sql = "SELECT notice_time, return_time, check_time FROM user WHERE id = :id";
@@ -19,7 +22,7 @@ try{
 }catch(Exception $e){
     $error[] =  "エラーが発生しました。";
 }
-
+*/
 ?>
 
 <!DOCTYPE html>
@@ -45,19 +48,20 @@ $user_id = $_SESSION['user_id'];
 
 if($_POST['m_time'] == 000000 OR $_POST['e_time'] == 000000){
 
-    $error[] = "朝と夜、どちらの時間も入力してください。";
+    $error[] = "出発時と更新時の時間は必ず入力してください。";
 }else{
 
-date_default_timezone_set('Asia/Tokyo');
 $notice_time = $_POST["m_time"];
 $check_time = $_POST["e_time"];
 $return_time = $_POST["r_time"];
 
-require_once("dbconnect.php");
-//SQLを実行
+require 'update_time.php';
+
+/*
 try{
     //入力された時間に変更するsql
-    $sql = "UPDATE user SET notice_time = :notice_time, return_time = :return_time, check_time = :check_time 
+    $sql = "UPDATE user
+    SET notice_time = :notice_time, return_time = :return_time, check_time = :check_time
     WHERE id = :id";
     //プリペアドステートメントを作る
     $stm = $pdo->prepare($sql);
@@ -76,6 +80,7 @@ try{
 }catch(Exception $e){
     $error[] =  "エラーが発生しました。";
  }
+ */
 }
 ?>
 
@@ -86,7 +91,7 @@ try{
             <i class="fa fa-envelope fa-lg fa-fw" aria-hidden="true"></i>
             その日の持ち物を通知する時間<br>
             (ex.通勤等で家を出る時間、持ち物を通知してほしい時間)<br>
-            <input type="time" name="m_time" value="<?= $mtime ?>" required><br>
+            <input type="time" name="m_time" value="<?= $mtime; ?>" required><br>
             </div>
 
             <div class="cp_iptxt">
@@ -94,14 +99,14 @@ try{
             家に帰りだす時間<br>
             (ex.会社や学校を出る時間)<br>
             <input type="time" name="r_time"><br>
-            <!-- <input type="time" name="r_time" value="<?= $rtime ?>" ><br> -->
+            <!-- <input type="time" name="r_time" value="$rtime;" ><br> -->
             </div>
 
             <div class="cp_iptxt">
             <i class="fa fa-envelope fa-lg fa-fw" aria-hidden="true"></i>
             次の日の持ち物を確認する時間<br>
             (ex.寝る前に持ち物の準備をする時間)<br>
-            <input type="time" name="e_time" value="<?= $etime ?>" required><br>
+            <input type="time" name="e_time" value="<?= $etime; ?>" required><br>
             </div>
 
             <div><button type="submit" name="send" class="button1">登録</button></div>
@@ -110,16 +115,19 @@ try{
             <div>
             <?php
 
-                if(empty($error)){
-                    echo "<HR>";
-                    echo "<h3>その日の持ち物を通知する時間<br>[$notice_time]  <br>次の日の持ち物を確認する時間<br>[$check_time]<br>で登録しました。<br><h3>";
+                if(empty($error)) :?>
+                    <HR>
+                    <h3>その日の持ち物を通知する時間<br>[<?=$notice_time?>]  <br>次の日の持ち物を確認する時間<br>[<?=$check_time?>]<br>で登録しました。<br><h3>
 
-                }else if(isset($_POST['send'])){
-                    echo "<HR>";
-                    foreach($error as $value){
-                        echo $value."<br>";
-                    }
-                }
+                <?php
+                else if(isset($_POST['send'])):?>
+                    <HR>
+                    <?php
+                    foreach($error as $value):?>
+                        <?=$value;?><br>
+                    <?php
+                    endforeach;
+                endif;
             ?>
 
             <br><a href='time_top.php' title='時間登録ページへ'>戻る</a><br>
