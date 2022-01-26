fac4cea86e39154093b4a06843c7d93b1ee97749
besurand <fujita.shun.t20ohf@gmail.com>
2022-01-26 15:16:01 +0900

    update_time.php変更

--- update_time.php
+++ update_time.php
@@ -1,23 +1,12 @@
 <?php
+require 'dbconnect.php';
+require 'DBController.php';
+$DBControl = new DBController();
+
 try {
     //入力された時間に変更
-    $sql = "UPDATE user
-    SET notice_time = :notice_time, return_time = :return_time, check_time = :check_time
-    WHERE id = :id";
-
-    $stm = $pdo->prepare($sql);
-    $stm->bindValue(':id', $_SESSION['user_id'], PDO::PARAM_INT);
-    $stm->bindValue(':notice_time', $notice_time, PDO::PARAM_INT);
-    if ($return_time == 000000) {
-        $stm->bindValue(':return_time', null, PDO::PARAM_NULL);
-    } else {
-        $stm->bindValue(':return_time', $return_time, PDO::PARAM_INT);
-    }
-    $stm->bindValue(':check_time', $check_time, PDO::PARAM_INT);
-    if ($stm->execute()) {
-    } else {
-        echo "時間登録でエラーが発生しました。";
-    }
+    $DBControl->setTime($_SESSION['user_id'], $notice_time, $return_time, $check_time);
+    
 } catch (Exception $e) {
     $error[] =  "エラーが発生しました。";
 }
