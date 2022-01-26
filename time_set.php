c34213c6d766ed6438c341a6e82b49a4393fb18d
WhiteF326 <Lume1a326.leno@gmail.com>
2022-01-26 20:13:34 +0900

    修正

--- time_set.php
+++ time_set.php
@@ -11,11 +11,11 @@ require 'search_time.php';
 <!DOCTYPE html>
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width,initial-scale=1">
-  <title>時間登録</title>
-  <link rel="stylesheet" href="style.css">
-  <link rel="icon" type="image/png" href="img/abcd2.png">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>通知時刻確認 / 変更</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/png" href="img/abcd2.png">
 
 </head>
 
@@ -25,7 +25,7 @@ require 'header_form.php';
 ?>
 
 
-<h2><span>時間登録</span></h2>
+<h2><span>通知時刻確認 / 変更</span></h2>
 
 <?php
 $error = [];
@@ -34,72 +34,72 @@ $user_id = $_SESSION['user_id'];
 
 if ($_POST['m_time'] == 000000 or $_POST['e_time'] == 000000) {
 
-  $error[] = "出発時と更新時の時間は必ず入力してください。";
+    $error[] = "出発時と更新時の時間は必ず入力してください。";
 } else {
-  $notice_time = $_POST["m_time"];
-  $check_time = $_POST["e_time"];
-  $return_time = $_POST["r_time"];
+    $notice_time = $_POST["m_time"];
+    $check_time = $_POST["e_time"];
+    $return_time = $_POST["r_time"];
 
-  require 'update_time.php';
+    require 'update_time.php';
 }
 ?>
 
 <section>
-  <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
 
-    <div class="cp_iptxt">
-      <i class="fa fa-envelope fa-lg fa-fw" aria-hidden="true"></i>
-      その日の持ち物を通知する時間<br>
-      (ex.通勤等で家を出る時間、持ち物を通知してほしい時間)<br>
-      <date><input type="time" name="m_time" value="<?= $mtime; ?>" required></date><br>
-    </div>
+        <div class="cp_iptxt">
+            <i class="fa fa-envelope fa-lg fa-fw" aria-hidden="true"></i>
+            その日の持ち物を通知する時間<br>
+            (ex.通勤等で家を出る時間、持ち物を通知してほしい時間)<br>
+            <date><input type="time" name="m_time" value="<?= $mtime; ?>" required></date><br>
+        </div>
 
-    <div class="cp_iptxt">
-      <i class="fa fa-envelope fa-lg fa-fw" aria-hidden="true"></i>
-      家に帰りだす時間<br>
-      (ex.会社や学校を出る時間)<br>
-      <date>
-        <input type="time" name="r_time">
-      </date><br>
-      <!-- <input type="time" name="r_time" value="<?= $rtime; ?>" ><br> -->
-    </div>
+        <div class="cp_iptxt">
+            <i class="fa fa-envelope fa-lg fa-fw" aria-hidden="true"></i>
+            家に帰りだす時間<br>
+            (ex.会社や学校を出る時間)<br>
+            <date>
+                <input type="time" name="r_time">
+            </date><br>
+            <!-- <input type="time" name="r_time" value="<?= $rtime; ?>" ><br> -->
+        </div>
 
-    <div class="cp_iptxt">
-      <i class="fa fa-envelope fa-lg fa-fw" aria-hidden="true"></i>
-      次の日の持ち物を確認する時間<br>
-      (ex.寝る前に持ち物の準備をする時間)<br>
-      <date>
-        <input type="time" name="e_time" value="<?= $etime; ?>" required>
-      </date><br>
-    </div>
+        <div class="cp_iptxt">
+            <i class="fa fa-envelope fa-lg fa-fw" aria-hidden="true"></i>
+            次の日の持ち物を確認する時間<br>
+            (ex.寝る前に持ち物の準備をする時間)<br>
+            <date>
+                <input type="time" name="e_time" value="<?= $etime; ?>" required>
+            </date><br>
+        </div>
 
-    <div><button type="submit" name="send" class="button1">登録</button></div>
-  </form>
+        <div><button type="submit" name="send" class="button1">登録</button></div>
+    </form>
 
-  <div>
-    <?php
-
-    if (empty($error)) : ?>
-      <HR>
-      <h3>その日の持ち物を通知する時間<br>[<?= $notice_time; ?>] <br>次の日の持ち物を確認する時間<br>[<?= $check_time; ?>]<br>で登録しました。<br>
-        <h3>
+    <div>
         <?php
-      elseif (isset($_POST['send'])) : ?>
-          <HR>
-          <?php
-          foreach ($error as $value) : ?>
-            <?= $value; ?><br>
 
-        <?php
-          endforeach;
-        endif;
-        ?>
+        if (empty($error)) : ?>
+            <HR>
+            <h3>その日の持ち物を通知する時間<br>[<?= $notice_time; ?>] <br>次の日の持ち物を確認する時間<br>[<?= $check_time; ?>]<br>で登録しました。<br>
+                <h3>
+                <?php
+            elseif (isset($_POST['send'])) : ?>
+                    <HR>
+                    <?php
+                    foreach ($error as $value) : ?>
+                        <?= $value; ?><br>
 
-        <br>
-        <div>
-          <button type="submit" name="send" class="button3">
-            <a href='time_top.php' title='時間登録ページへ' class="a">戻る</a></button>
-        </div><br>
+                <?php
+                    endforeach;
+                endif;
+                ?>
 
-  </div>
+                <br>
+                <div>
+                    <button type="submit" name="send" class="button3">
+                        <a href='time_top.php' title='通知時刻確認 / 変更ページへ' class="a">戻る</a></button>
+                </div><br>
+
+    </div>
 </section>
--- time_top.php
+++ time_top.php
@@ -4,98 +4,122 @@
 <html lang="ja">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>時間登録</title>
-  <link rel="stylesheet" media="all" href="css/ress.min.css" />
-  <link rel="stylesheet" media="all" href="css/style.css" />
-  <script src="js/jquery-2.1.4.min.js"></script>
-  <script src="js/style.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>通知時刻確認 / 変更</title>
+    <link rel="stylesheet" media="all" href="css/ress.min.css" />
+    <link rel="stylesheet" media="all" href="css/style.css" />
+    <!-- <script src="js/jquery-2.1.4.min.js"></script> -->
+    <script src="js/style.js"></script>
 
-  <link rel="icon" type="image/png" href="img/abcd2.png">
-
+    <link rel="icon" type="image/png" href="img/abcd2.png" />
 </head>
 
+<script type="text/javascript">
+    window.addEventListener("load", () => {
+        document.getElementById("modifyTimeSettings").onclick = () => {
+            console.log("test");
+            location.href = "time_set.php";
+        }
+    });
+</script>
+
+<?php
+// strings declaration
+$time_names = array(
+    "notice_time" => "その日の持ち物を通知する時間",
+    "return_time" => "帰りだす時間",
+    "check_time" => "次の日の持ち物を確認する時間"
+);
+?>
+
 <body>
+    <?php
+    // ヘッダを表示
+    include 'header_top.php';
+    ?>
 
-  <?php
-  //ヘッダ表示
-  include 'header_top.php';
-  ?>
-
-  <div class="mainimg">
-    <img src="img/23.png" alt="サブページ画像">
-  </div>
-
-  <main>
-    <article>
-      <div class="container">
-        <div class="row">
-          <div class="col span-8">
-            <div class="breadcrumb">
-              <ul>
-                <li><a href="index.php">ホーム</a> > <a href="confirm.php?id=<?php echo $_SESSION['user_id']; ?>">登録物一覧</a> > 時間登録</li>
-              </ul>
-            </div>
-
-            <h2 class="underline">時間登録</h2>
-            <?php
-            require 'dbconnect.php';
-            require 'search_time.php';
-
-            if (!empty($result["notice_time"] && !empty($result["check_time"]))) : ?>
-              現在設定されている時間<br>
-              その日の持ち物を通知する時間 : <?= $result["notice_time"]; ?><br>
-              <?php
-              if (($result["return_time"]) != null) : ?>
-                帰りだす時間 　　　　　　　　: <?= $result["return_time"]; ?><br>
-              <?php
-
-              else : ?>
-                帰りだす時間 　　　　　　　　: 未設定　<br>
-              <?php
-              endif; ?>
-              次の日の持ち物を確認する時間 : <?= $result["check_time"]; ?><br>
-            <?php
-            else : ?>
-              持ち物を通知する時間を登録してください。<br>
-            <?php
-            endif; ?>
-            <hr>
-            
-            <div>
-              <button type="submit" name="send" class="button1">
-                <a href="time_set.php">時間を設定する</a>
-              </button>
-            </div>
-
-          </div>
-
-          <div class="col span-4">
-            <a href="confirm.php?id=<?php echo $_SESSION['user_id']; ?>">
-              <img src="img/2.png" alt="バナー画像">
-            </a>
-          </div>
-        </div>
-      </div>
-    </article>
-  </main>
-
-  <?php
-  include "footer.php";
-  ?>
-
-  <div class="copyright">
-    <div class="container">
-      <div class="row">
-        <div class="col">
-          Fukuiohr2021 © <a href="https://fukuiohr2.sakura.ne.jp/2021/wasurenai/index.php" target="_blank">Wasurenai </a>
-
-        </div>
-      </div>
+    <div class="mainimg">
+        <img src="img/23.png" alt="サブページ画像">
     </div>
-  </div>
-  <p id="pagetop"><a href="#">TOP</a></p>
+
+    <main>
+        <article>
+            <div class="container">
+                <div class="row">
+                    <div class="col span-8">
+                        <div class="breadcrumb">
+                            <ul>
+                                <li><a href="index.php">ホーム</a> > 通知時刻確認 / 変更</li>
+                            </ul>
+                        </div>
+
+                        <h2 class="underline">通知時刻確認 / 変更</h2>
+
+                        <h5>現在設定されている時間</h5>
+
+                        <?php
+                        // ユーザが登録している時刻を取得
+                        require "dbconnect.php";
+                        require "search_time.php";
+                        /*
+                        return in:
+                        {
+                            "notice_time" => 通知時刻,
+                            "return_time" => 帰りの時刻,
+                            "check_time" => 更新時刻
+                        }
+                        */
+                        // 通知時間を表示するテーブルを表示
+                        ?>
+                        <table>
+                            <?php
+                            foreach ($result as $key => $value) : ?>
+                                <tr>
+                                    <td><?= $time_names[$key] ?></td>
+                                    <td>/</td>
+                                    <td>
+                                        <?= $value ? date(
+                                            "H時 i分 s秒",
+                                            strtotime($value)
+                                        ) : "登録されていません" ?>
+                                    </td>
+                                </tr>
+                            <?php endforeach;
+                            ?>
+                        </table>
+
+                        <div>
+                            <button id="modifyTimeSettings" class="button1">
+                                時刻を変更
+                            </button>
+                        </div>
+                    </div>
+
+                    <div class="col span-4">
+                        <a href="confirm.php?id=<?php echo $_SESSION['user_id']; ?>">
+                            <img src="img/2.png" alt="バナー画像">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </article>
+    </main>
+
+    <?php
+    include "footer.php";
+    ?>
+
+    <div class="copyright">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    Fukuiohr2021 © <a href="https://fukuiohr2.sakura.ne.jp/2021/wasurenai/index.php" target="_blank">Wasurenai </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <p id="pagetop"><a href="#">TOP</a></p>
 </body>
 
 </html>
